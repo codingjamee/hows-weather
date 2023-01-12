9545b57034f712f1e0789c4cb3bf9366fe600734
jamee <codingjamee@gmail.com>
2023-01-12 16:11:33 +0900

    카카오맵 띄우기

---
+++ src/components/Map.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import classes from "./Map.module.css";
+const { kakao } = window;
+
+let latitude;
+let logitude;
+navigator.geolocation.getCurrentPosition(function (position) {
+  latitude = position.coords.latitude;
+  logitude = position.coords.longitude;
+  console.log(latitude);
+  console.log(logitude);
+});
+const Map = () => {
+  useEffect(() => {
+    const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(latitude, logitude), //지도의 중심좌표.
+      level: 7, //지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    //마커가 표시 될 위치
+    let markerPosition = new kakao.maps.LatLng(latitude, logitude);
+
+    // 마커를 생성
+    let marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    // 마커를 지도 위에 표시
+    marker.setMap(map);
+  }, []);
+
+  return <div id="map" className={classes.map}></div>;
+};
+export default Map;
